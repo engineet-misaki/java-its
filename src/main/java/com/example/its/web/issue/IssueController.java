1573f5b13e6a5877d5f42df9944143ca0b9a3ea0
MISAKI\toyam <test-mail@test.com>
2025-07-03 18:31:05 +0900

    h2追加、課題作成画面追加

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,24 +1,31 @@
 package com.example.its.web.issue;
 
 import com.example.its.domain.issue.IssueEntity;
+import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/issues")
 public class IssueController {
 
-    // Get /issue
-    @GetMapping("/issues")
+    private final IssueService issueService;
+
+    @GetMapping
     public String showList(Model model) {
-        var issueList = List.of(
-                new IssueEntity(1, "概要1", "説明1"),
-                new IssueEntity(2, "概要2", "説明2"),
-                new IssueEntity(3, "概要3", "説明3")
-        );
-        model.addAttribute("issueList",issueList);
+
+        model.addAttribute("issueList",issueService.findAll());
         return "issues/list";
+    }
+
+    @GetMapping("/creationForm")
+    public String showCreationForm() {
+        return "issues/creationForm";
     }
 }
